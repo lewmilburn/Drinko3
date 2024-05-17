1f3845fac842a1421a891848cc0e8d4574952ba5
Lewis Milburn <lewiscmilburn@gmail.com>
2024-05-17 16:13:03 +0100

    Change database system.

--- processes/database.js
+++ processes/database.js
@@ -1,17 +1,22 @@
 module.exports = function(log) {
-    let mysql = require('mysql');
+    try {
+        let mysql = require('mysql');
 
-    let con = mysql.createConnection({
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS
-    });
+        let con = mysql.createConnection({
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS
+        });
 
-    con.connect(function(err) {
-        if (err) throw err;
-        log.info(`[STARTUP] Database started, connected to "`+process.env.DB_NAME+`"`);
-    });
+        con.connect(function (err) {
+            log.info('[DB][500] Error connecting to the database - '+err);
+            log.info(`[DB][200] Database started, connected to "` + process.env.DB_NAME + `"`);
+        });
 
-    return con;
+        return con;
+    } catch (e) {
+        log.info('[DB][500] Error connecting to the database - '+e);
+        socket.emit('error', e);
+    }
 }
--- processes/socket.js
+++ processes/socket.js
@@ -22,29 +22,24 @@ module.exports = function (io, webport, rooms, log) {
             name = he.escape(name);
             let room = Math.floor(100000 + Math.random() * 900000);
 
-            try {
-                let db = require(__dirname + '/database')(log);
-                db.query("INSERT INTO `games` (`id`, `gameID`, `host`) VALUES (NULL, '"+room+"', "+db.escape(name)+")", function(error) {
-                    if (error) {
-                        log.info('[SQL][500] Executed query :'+"INSERT INTO `games` (`id`, `gameID`, `host`) VALUES (NULL, '"+room+"', "+db.escape(name)+")");
-                        log.info('[SQL][500] Error executing query:', error);
-                        socket.emit('error', error);
-                    } else {
-                        log.info('[SQL][200] Executed query :'+"INSERT INTO `games` (`id`, `gameID`, `host`) VALUES (NULL, '"+room+"', "+db.escape(name)+")");
-                    }
+            let db = require(__dirname + '/database')(log);
+            db.query("INSERT INTO `games` (`id`, `gameID`, `host`) VALUES (NULL, '" + room + "', " + db.escape(name) + ")", function (error) {
+                if (error) {
+                    log.info('[DB][500] Executed query :' + "INSERT INTO `games` (`id`, `gameID`, `host`) VALUES (NULL, '" + room + "', " + db.escape(name) + ")");
+                    log.info('[DB][500] Error executing query:', error);
+                    socket.emit('error', error);
+                } else {
+                    log.info('[DB][200] Executed query :' + "INSERT INTO `games` (`id`, `gameID`, `host`) VALUES (NULL, '" + room + "', " + db.escape(name) + ")");
+                }
 
-                    db.end();
+                db.end();
 
-                    socket.emit('goToRoom',room);
+                socket.emit('goToRoom', room);
 
-                    log.info('[SOCKET][200] Created room '+room);
-                });
-            } catch (e) {
-                log.info('Error connecting '+name+' to the game - '+e);
-                socket.emit('error', e);
-            }
-        })
+                log.info('[SOCKET][200] Created room ' + room);
+            });
+        });
     });
 
-    log.info(`[STARTUP][200] Socket started`);
+    log.info(`[SOCKET][200] Socket started`);
 }
